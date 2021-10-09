3d5561595167062809287afefe166fcd8d7cd424
l0rdcafe <ismailarafa18@gmail.com>
2021-10-09 15:39:25 -0400

    more features

--- main.js
+++ main.js
@@ -5,16 +5,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const play = document.getElementById("play");
   const stop = document.getElementById("stop");
+  const frequency = document.getElementById("frequency");
+  const detune = document.getElementById("detune");
+  const freq = document.getElementById("freq");
+  const det = document.getElementById("det");
+
+  frequency.addEventListener("change", (e) => {
+    freq.innerHTML = `${e.target.value} Hz`;
+  });
+
+  detune.addEventListener("change", (e) => {
+    det.innerHTML = e.target.value;
+  });
 
   play.addEventListener("click", () => {
+    if (osc != null) {
+      osc.stop();
+    }
+
     osc = audioCtx.createOscillator();
     const waveType = document.querySelector('input[name="wave"]:checked').value;
+    console.log({ frequency: frequency.value, detune: detune.value, waveType });
     osc.type = waveType;
+    osc.frequency.value = frequency.value;
+    osc.detune.value = detune.value;
     osc.connect(audioCtx.destination);
     osc.start(audioCtx.currentTime);
   });
 
   stop.addEventListener("click", () => {
-    osc.stop();
+    if (osc != null) {
+      osc.stop();
+    }
   });
 });
