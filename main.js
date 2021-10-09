a76b8faf8013ec32977720c4e95614fddb0100e3
l0rdcafe <ismailarafa18@gmail.com>
2021-10-09 15:59:00 -0400

    add gain selector

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const audioCtx = new AudioContext();
 
   let osc;
+  let gainOsc;
 
   const play = document.getElementById("play");
   const stop = document.getElementById("stop");
   const frequency = document.getElementById("frequency");
   const detune = document.getElementById("detune");
+  const gain = document.getElementById("gain");
   const freq = document.getElementById("freq");
   const det = document.getElementById("det");
+  const vol = document.getElementById("vol");
+
+  gain.addEventListener("change", (e) => {
+    if (gainOsc != null) {
+      gainOsc.gain.value = e.target.value / 100;
+    }
+    vol.innerHTML = e.target.value / 100;
+  });
 
   frequency.addEventListener("change", (e) => {
     if (osc != null) {
@@ -30,11 +40,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     osc = audioCtx.createOscillator();
+    gainOsc = audioCtx.createGain();
     const waveType = document.querySelector('input[name="wave"]:checked').value;
     osc.type = waveType;
     osc.frequency.value = frequency.value;
     osc.detune.value = detune.value;
-    osc.connect(audioCtx.destination);
+    osc.connect(gainOsc);
+    gainOsc.gain.value = gain.value;
+    gainOsc.connect(audioCtx.destination);
     osc.start(audioCtx.currentTime);
   });
 
